263dc6b22e9ea58e2fc7de7abe4feda3a5d38b2d
Wretchman <hab4791@gmail.com>
2018-07-30 21:07:13 -0400

    some code added

--- src/test/java/abc_practices/GoogleFooterLinks.java
+++ src/test/java/abc_practices/GoogleFooterLinks.java
@@ -1,12 +1,15 @@
 package abc_practices;
 
+<<<<<<< HEAD
 import static org.junit.Assert.assertEquals;
 
+=======
+>>>>>>> 780f2c1b4088cc29cbbb8016acee5740495fca98
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.testng.Assert;
+//import static org.junit.Assert.assertEquals;
 
 public class GoogleFooterLinks {
 
@@ -22,12 +25,13 @@ public class GoogleFooterLinks {
 		Thread.sleep(2000);
 		
 		if(driver.getPageSource().contains("Privacy"))
-		System.out.println("The test passed");
+			System.out.println("The test passed");
 		else
 			System.out.println("The test failed");
 		
 		Assert.assertEquals(true,driver.getPageSource().contains("Privacy"));
 		Assert.assertEquals(0,2-2);
+<<<<<<< HEAD
 		driver.navigate().back();
 		
 		driver.findElement(By.linkText("Terms")).click();
@@ -49,6 +53,10 @@ public class GoogleFooterLinks {
 		
 		Thread.sleep(4000);
 		
+=======
+
+		Thread.sleep(5000);
+>>>>>>> 780f2c1b4088cc29cbbb8016acee5740495fca98
 		driver.close();
 		
 		
