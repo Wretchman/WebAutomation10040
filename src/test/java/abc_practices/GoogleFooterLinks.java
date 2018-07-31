737a18c4aa10207e484c5e565fe491021513600a
Wretchman <hab4791@gmail.com>
2018-07-30 22:18:39 -0400

    some program added

--- src/test/java/abc_practices/GoogleFooterLinks.java
+++ src/test/java/abc_practices/GoogleFooterLinks.java
@@ -1,37 +1,30 @@
 package abc_practices;
 
-<<<<<<< HEAD
-import static org.junit.Assert.assertEquals;
-
-=======
->>>>>>> 780f2c1b4088cc29cbbb8016acee5740495fca98
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-//import static org.junit.Assert.assertEquals;
 
 public class GoogleFooterLinks {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+        
+        WebDriver driver = new ChromeDriver();  // You can use any other web browser.
+        driver.get("https://www.google.com/");
 		Thread.sleep(2000);
 		
 		driver.findElement(By.linkText("Privacy")).click();
-		Thread.sleep(2000);
 		
 		if(driver.getPageSource().contains("Privacy"))
 			System.out.println("The test passed");
 		else
 			System.out.println("The test failed");
 		
-		Assert.assertEquals(true,driver.getPageSource().contains("Privacy"));
-		Assert.assertEquals(0,2-2);
-<<<<<<< HEAD
+		Assert.assertEquals(true, driver.getPageSource().contains("Privacy"));
+		Assert.assertEquals(0, 2-2);
+		Thread.sleep(2000);
 		driver.navigate().back();
 		
 		driver.findElement(By.linkText("Terms")).click();
@@ -42,35 +35,22 @@ public class GoogleFooterLinks {
 		Assert.assertEquals(true, driver.getPageSource().contains("Terms"));
 		Thread.sleep(2000);
 		driver.navigate().back();
-		
 		driver.findElement(By.linkText("Settings")).click();
 		if(driver.getPageSource().contains("Settings"))
 			System.out.println("The test passed");
 		else
 			System.out.println("The test failed");
 		Assert.assertEquals(true, driver.getPageSource().contains("Settings"));
-		Assert.assertEquals(3, 5-2);
-		
-		Thread.sleep(4000);
-		
-=======
-
-		Thread.sleep(5000);
->>>>>>> 780f2c1b4088cc29cbbb8016acee5740495fca98
+		Thread.sleep(2000);
 		driver.close();
 		
 		
 		
-	
-
-	
+		
+		
+			
+		
+		
 	}
 
-	}
-
-
-
-	
-	
-
-
+}
